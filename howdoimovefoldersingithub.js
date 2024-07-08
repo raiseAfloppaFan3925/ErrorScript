0822284870a8a3d08dff4b7c10795d1ceb8b4813
raiseAfloppaFan3925 <130119292+raiseAfloppaFan3925@users.noreply.github.com>
2024-07-08 21:22:01 +0800

    Clarify the joke
    Just in case people don't understand howdoimovefoldersingithub.js

--- howdoimovefoldersingithub.js
+++ howdoimovefoldersingithub.js
@@ -1,3 +1,15 @@
 // How do I move folders in GitHub?
 
 // I guess it's only the vs code
+
+export class Joke {
+  text : string;
+
+  constructor ( text : string ) {
+    this.text = text;
+  }
+}
+
+let joke = new Joke( `The joke is that I didn't know how to move folders in GitHub.` );
+
+const NOTICE = `Please ignore this file, as it was meant to be a ${joke}, I didn't actually mean for this to be a serious one just like pythonisannoying.md. Ignore that one too please`;
